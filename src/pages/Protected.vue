d54ce613f55de2f4033e255da3d0956e6236ea54
Russell Chapman <chpmnrssll@gmail.com>
2019-05-17 01:56:58 -0600

    Debugging Netlify build error

--- src/pages/Protected.vue
+++ src/pages/Protected.vue
@@ -5,7 +5,9 @@
 </template>
 
 <script>
-const isClient = typeof window !== 'undefined';
+import netlifyIdentity from 'netlify-identity-widget';
+
+// const isClient = typeof window !== 'undefined';
 
 export default {
   name: 'Protected',
@@ -15,17 +17,20 @@ export default {
         return next();
       }
       // alert('Please log in or sign up!');
-      if (isClient) {
-        vm.$bvModal.msgBoxOk('Please log in or sign up!', {
-          title: 'Error',
-          size: 'sm',
-          buttonSize: 'sm',
-          okVariant: 'danger',
-          headerClass: 'p-2 border-bottom-0',
-          footerClass: 'p-2 border-top-0',
-          centered: true,
-        });
-      }
+      // if (isClient) {
+      //   setTimeout(() => {
+      //     vm.$bvModal.msgBoxOk('Please log in or sign up!', {
+      //       title: 'Error',
+      //       size: 'sm',
+      //       buttonSize: 'sm',
+      //       okVariant: 'danger',
+      //       headerClass: 'p-2 border-bottom-0',
+      //       footerClass: 'p-2 border-top-0',
+      //       centered: true,
+      //     });
+      //   }, 300);
+      // }
+      netlifyIdentity.open();
       return next('/');
     });
   },
