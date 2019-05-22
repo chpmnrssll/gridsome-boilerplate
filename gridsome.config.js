53e1161b8c9dfeb4c707aa2aae1e304c81919950
Russell Chapman <chpmnrssll@gmail.com>
2019-05-22 01:40:06 -0600

    Update home with carousel component and debugging vueLoader.options.transformAssetUrls for bootstrap-vue components

--- gridsome.config.js
+++ gridsome.config.js
@@ -34,6 +34,25 @@ module.exports = {
       },
     },
   ],
+  configureWebpack: {
+    vueLoader: {
+      options: {
+        transformAssetUrls: {
+          video: ['src', 'poster'],
+          source: 'src',
+          img: 'src',
+          image: 'xlink:href',
+          'b-img': 'src',
+          'b-img-lazy': ['src', 'blank-src'],
+          'b-card': 'img-src',
+          'b-card-img': 'img-src',
+          'b-card-img-lazy': ['src', 'blank-src'],
+          'b-carousel-slide': 'img-src',
+          'b-embed': 'src',
+        },
+      },
+    },
+  },
   css: {
     loaderOptions: {
       scss: {},
