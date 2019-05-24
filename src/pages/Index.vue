8f08ba538fb6c5bb9108e67c9437f3e24c852d29
Russell Chapman <chpmnrssll@gmail.com>
2019-05-24 01:16:09 -0600

    Updated layout/styling on home page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -69,18 +69,125 @@
 
     <h3 class="mt-5 mb-4">Features</h3>
     <p>This starter project includes some of the latest powerful technologies.</p>
-    <ul>
-      <li><b>Gridsome - </b> Vue.js & GraphQL powered framework genrating static files.</li>
-      <li>
-        <b>Bootstrap & Bootstrap Vue - </b> Powerful styling and layout with styled Vue components.
-      </li>
-      <li><b>Sass - </b> Professional grade CSS extension with many features.</li>
-      <li><b>Markdown - </b> Easy and widely used format for writing content.</li>
-      <li><b>Netlify CMS - </b> CMS that uses the Github repo for storage.</li>
-      <li><b>Netlify forms - </b> Netlify forms ready to be used after deployment.</li>
-      <li><b>Google Analytics - </b> Just add your tracking ID in the config.</li>
-      <li><b>Sitemap generator - </b> Automatically generates a sitemap.xml file.</li>
-    </ul>
+
+    <b-card-group deck>
+      <b-card img-src="../../uploads/code-review.svg" img-alt="Built with" img-top>
+        <b-card-title>Built with</b-card-title>
+        <b-list-group flush>
+          <b-list-group-item
+            href="https://gridsome.org/"
+            v-b-tooltip.hover.right
+            title="A framework for JAMstack websites & PWAs based on Vue.js"
+          >
+            Gridsome
+          </b-list-group-item>
+          <b-list-group-item
+            href="https://graphql.org/"
+            v-b-tooltip.hover.right
+            title="A powerful data query language for APIs that uses your existing data"
+          >
+            GraphQL
+          </b-list-group-item>
+          <b-list-group-item
+            href="https://vuejs.org/"
+            v-b-tooltip.hover.right
+            title="A JavaScript framework for building user interfaces and single-page applications"
+          >
+            Vue.js
+          </b-list-group-item>
+          <b-list-group-item
+            href="https://vuex.vuejs.org/"
+            v-b-tooltip.hover.right
+            title="A state management pattern & library for Vue.js applications"
+          >
+            Vuex
+          </b-list-group-item>
+        </b-list-group>
+      </b-card>
+
+      <b-card img-src="../../uploads/profile.svg" img-alt="Authoring" img-top>
+        <b-card-title>Authoring</b-card-title>
+        <b-list-group flush>
+          <b-list-group-item
+            href="https://www.markdownguide.org/"
+            v-b-tooltip.hover.right
+            title="A lightweight markup language with plain text formatting syntax that can be converted to many output formats"
+          >
+            Markdown
+          </b-list-group-item>
+          <b-list-group-item
+            href="https://www.netlifycms.org/"
+            v-b-tooltip.hover.right
+            title="A CMS that gives non-technical users a simple way to edit and add content to any site built with a static site generator"
+          >
+            Netlify CMS
+          </b-list-group-item>
+          <b-list-group-item
+            href="https://www.netlify.com/docs/identity/"
+            v-b-tooltip.hover.right
+            title="A full suite of authentication functionality, backed by the GoTrue API"
+          >
+            Netlify Identity
+          </b-list-group-item>
+        </b-list-group>
+      </b-card>
+
+      <b-card img-src="../../uploads/responsive.svg" img-alt="Responsive Design" img-top>
+        <b-card-title>Styling</b-card-title>
+        <b-list-group flush>
+          <b-list-group-item
+            href="https://sass-lang.com/"
+            v-b-tooltip.hover.right
+            title="A mature, stable, and powerful professional grade CSS extension language"
+          >
+            Sass
+          </b-list-group-item>
+          <b-list-group-item
+            href="https://bootstrap-vue.js.org/"
+            v-b-tooltip.hover.right
+            title="One of the most comprehensive implementations of the Bootstrap 4.3 component and grid system for Vue.js"
+          >
+            Bootstrap Vue
+          </b-list-group-item>
+          <b-list-group-item
+            href="https://gridsome.org/plugins/@gridsome/plugin-critical"
+            v-b-tooltip.hover.right
+            title="Extracts & inlines critical-path (above-the-fold) CSS"
+          >
+            Critical path CSS extraction
+          </b-list-group-item>
+        </b-list-group>
+      </b-card>
+    </b-card-group>
+
+    <b-card-group deck>
+      <b-card img-src="../../uploads/adjustments.svg" img-alt="Data" img-left>
+        <b-card-title>Data</b-card-title>
+        <b-list-group flush>
+          <b-list-group-item
+            href="https://www.netlify.com/docs/form-handling/"
+            v-b-tooltip.hover.right
+            title="Form submission handling provided by Netlify"
+          >
+            Netlify Forms
+          </b-list-group-item>
+          <b-list-group-item
+            href="https://analytics.google.com/analytics/web/"
+            v-b-tooltip.hover.right
+            title="A web analytics service offered by Google that tracks and reports website traffic"
+          >
+            Google Analytics
+          </b-list-group-item>
+          <b-list-group-item
+            href="https://gridsome.org/plugins/@gridsome/plugin-sitemap"
+            v-b-tooltip.hover.right
+            title="Automatic sitemap generation for Gridsome sites"
+          >
+            Sitemap generator
+          </b-list-group-item>
+        </b-list-group>
+      </b-card>
+    </b-card-group>
   </Layout>
 </template>
 
@@ -92,7 +199,32 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+.card {
+  box-shadow: 2px 3px 4px #888;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    box-shadow: 4px 6px 8px #aaa;
+    transform: scale(1.025);
+  }
+}
+
+.card-deck {
+  margin-bottom: 2rem;
+}
+
+.card-img-top {
+  padding: 1rem;
+}
+
+.card-img-left {
+  background-size: cover;
+  height: 100%;
+  padding: 1rem;
+  width: 50%;
+}
+
 .carousel-image {
   height: 75vh;
   width: 100%;
