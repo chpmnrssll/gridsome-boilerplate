8b1b51c39930f1579794293d1b22b2ff668e3a95
Russell Chapman <chpmnrssll@gmail.com>
2019-05-25 02:40:57 -0600

    Added svg separator component

---
+++ src/components/SVGPolygon.vue
@@ -0,0 +1,93 @@
+<template>
+  <svg>
+    <defs>
+      <linearGradient id="header-gradient" x2="0.35" y2="1">
+        <stop offset="0%" :stop-color="gradient.start" />
+        <stop offset="100%" :stop-color="gradient.stop" />
+      </linearGradient>
+      <filter id="dropshadow" height="150%">
+        <feDropShadow />
+      </filter>
+    </defs>
+    <polygon :points="points" />
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    height: {
+      type: Number,
+      default: 0,
+    },
+    triangles: {
+      type: Number,
+      default: 8,
+    },
+    width: {
+      type: Number,
+      default: window.innerWidth,
+    },
+  },
+  data() {
+    return {
+      gradient: {
+        start: '#ffffff00',
+        stop: '#91e5cbaa',
+      },
+      pointData: [],
+    };
+  },
+  computed: {
+    viewbox() {
+      return `0 0 ${this.width} ${this.height}`;
+    },
+    triangleSize() {
+      return this.width / this.triangles;
+    },
+    points() {
+      return this.pointData.map(point => `${point.x},${point.y}`).join(' ');
+    },
+  },
+  watch: {
+    height() {
+      this.buildTriangles();
+    },
+  },
+  methods: {
+    buildTriangles() {
+      this.pointData.push({ x: 0, y: 0 });
+
+      const halfSize = this.triangleSize / 2;
+      for (let i = 0; i < this.triangles; i += 1) {
+        let x = i * this.triangleSize;
+
+        this.pointData.push({ x, y: this.height });
+        x += halfSize;
+        this.pointData.push({ x, y: this.height + halfSize });
+        x += halfSize;
+        this.pointData.push({ x, y: this.height });
+      }
+
+      this.pointData.push({ x: this.width, y: 0 });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+svg {
+  height: 200%;
+  left: 0;
+  overflow-x: hidden;
+  position: absolute;
+  top: 0;
+  width: 100%;
+  z-index: -1;
+}
+
+polygon {
+  fill: url(#header-gradient);
+  filter: url(#dropshadow);
+}
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,7 +5,6 @@
       :interval="5000"
       controls
       indicators
-      background="#ababab"
       style="text-shadow: 1px 1px 2px #333;"
     >
       <b-carousel-slide>
@@ -54,145 +53,167 @@
       </b-carousel-slide>
     </b-carousel>
 
-    <h3 class="mt-5 mb-4">About</h3>
-    <p>
-      This is a JAMstack boilerplate using Gridsome that comes with many features out of the box. It
-      utilizes Bootstrap that is well known for powerful styling and layout. Ready to be deployed,
-      automagically imports data to CMS and sets up your /admin page for easy access to edit your
-      content.
-    </p>
+    <section id="about">
+      <h3 class="mt-5 mb-4">About</h3>
+      <p>
+        This is a JAMstack boilerplate using Gridsome that comes with many features out of the box.
+        It utilizes Bootstrap that is well known for powerful styling and layout. Ready to be
+        deployed, automagically imports data to CMS and sets up your /admin page for easy access to
+        edit your content.
+      </p>
+      <p>
+        It is designed to use Netlify and Github which all have very fair free tier to their
+        services, so you wont need to pay for monthly subscriptions and costly hosting fees.
+      </p>
+    </section>
 
-    <p>
-      It is designed to use Netlify and Github which all have very fair free tier to their services,
-      so you wont need to pay for monthly subscriptions and costly hosting fees.
-    </p>
+    <section id="features">
+      <h3 class="mt-5 pt-mb-4">Features</h3>
+      <p>This starter project includes some of the latest powerful technologies.</p>
 
-    <h3 class="mt-5 mb-4">Features</h3>
-    <p>This starter project includes some of the latest powerful technologies.</p>
+      <b-card-group deck>
+        <b-card img-src="../../uploads/code-review.svg" img-alt="Built with" img-top>
+          <b-card-title>Built with</b-card-title>
+          <b-list-group flush>
+            <b-list-group-item
+              href="https://gridsome.org/"
+              v-b-tooltip.hover.right
+              title="A framework for JAMstack websites & PWAs based on Vue.js"
+            >
+              Gridsome
+            </b-list-group-item>
+            <b-list-group-item
+              href="https://graphql.org/"
+              v-b-tooltip.hover.right
+              title="A powerful data query language for APIs that uses your existing data"
+            >
+              GraphQL
+            </b-list-group-item>
+            <b-list-group-item
+              href="https://vuejs.org/"
+              v-b-tooltip.hover.right
+              title="A JavaScript framework for building user interfaces and single-page applications"
+            >
+              Vue.js
+            </b-list-group-item>
+            <b-list-group-item
+              href="https://vuex.vuejs.org/"
+              v-b-tooltip.hover.right
+              title="A state management pattern & library for Vue.js applications"
+            >
+              Vuex
+            </b-list-group-item>
+          </b-list-group>
+        </b-card>
 
-    <b-card-group deck>
-      <b-card img-src="../../uploads/code-review.svg" img-alt="Built with" img-top>
-        <b-card-title>Built with</b-card-title>
-        <b-list-group flush>
-          <b-list-group-item
-            href="https://gridsome.org/"
-            v-b-tooltip.hover.right
-            title="A framework for JAMstack websites & PWAs based on Vue.js"
-          >
-            Gridsome
-          </b-list-group-item>
-          <b-list-group-item
-            href="https://graphql.org/"
-            v-b-tooltip.hover.right
-            title="A powerful data query language for APIs that uses your existing data"
-          >
-            GraphQL
-          </b-list-group-item>
-          <b-list-group-item
-            href="https://vuejs.org/"
-            v-b-tooltip.hover.right
-            title="A JavaScript framework for building user interfaces and single-page applications"
-          >
-            Vue.js
-          </b-list-group-item>
-          <b-list-group-item
-            href="https://vuex.vuejs.org/"
-            v-b-tooltip.hover.right
-            title="A state management pattern & library for Vue.js applications"
-          >
-            Vuex
-          </b-list-group-item>
-        </b-list-group>
-      </b-card>
+        <b-card img-src="../../uploads/profile.svg" img-alt="Authoring" img-top>
+          <b-card-title>Authoring</b-card-title>
+          <b-list-group flush>
+            <b-list-group-item
+              href="https://www.markdownguide.org/"
+              v-b-tooltip.hover.right
+              title="A lightweight markup language with plain text formatting syntax that can be converted to many output formats"
+            >
+              Markdown
+            </b-list-group-item>
+            <b-list-group-item
+              href="https://www.netlifycms.org/"
+              v-b-tooltip.hover.right
+              title="A CMS that gives non-technical users a simple way to edit and add content to any site built with a static site generator"
+            >
+              Netlify CMS
+            </b-list-group-item>
+            <b-list-group-item
+              href="https://www.netlify.com/docs/identity/"
+              v-b-tooltip.hover.right
+              title="A full suite of authentication functionality, backed by the GoTrue API"
+            >
+              Netlify Identity
+            </b-list-group-item>
+          </b-list-group>
+        </b-card>
 
-      <b-card img-src="../../uploads/profile.svg" img-alt="Authoring" img-top>
-        <b-card-title>Authoring</b-card-title>
-        <b-list-group flush>
-          <b-list-group-item
-            href="https://www.markdownguide.org/"
-            v-b-tooltip.hover.right
-            title="A lightweight markup language with plain text formatting syntax that can be converted to many output formats"
-          >
-            Markdown
-          </b-list-group-item>
-          <b-list-group-item
-            href="https://www.netlifycms.org/"
-            v-b-tooltip.hover.right
-            title="A CMS that gives non-technical users a simple way to edit and add content to any site built with a static site generator"
-          >
-            Netlify CMS
-          </b-list-group-item>
-          <b-list-group-item
-            href="https://www.netlify.com/docs/identity/"
-            v-b-tooltip.hover.right
-            title="A full suite of authentication functionality, backed by the GoTrue API"
-          >
-            Netlify Identity
-          </b-list-group-item>
-        </b-list-group>
-      </b-card>
+        <b-card img-src="../../uploads/responsive.svg" img-alt="Responsive Design" img-top>
+          <b-card-title>Styling</b-card-title>
+          <b-list-group flush>
+            <b-list-group-item
+              href="https://sass-lang.com/"
+              v-b-tooltip.hover.right
+              title="A mature, stable, and powerful professional grade CSS extension language"
+            >
+              Sass
+            </b-list-group-item>
+            <b-list-group-item
+              href="https://bootstrap-vue.js.org/"
+              v-b-tooltip.hover.right
+              title="One of the most comprehensive implementations of the Bootstrap 4.3 component and grid system for Vue.js"
+            >
+              Bootstrap Vue
+            </b-list-group-item>
+            <b-list-group-item
+              href="https://gridsome.org/plugins/@gridsome/plugin-critical"
+              v-b-tooltip.hover.right
+              title="Extracts & inlines critical-path (above-the-fold) CSS"
+            >
+              Critical path CSS extraction
+            </b-list-group-item>
+          </b-list-group>
+        </b-card>
+      </b-card-group>
 
-      <b-card img-src="../../uploads/responsive.svg" img-alt="Responsive Design" img-top>
-        <b-card-title>Styling</b-card-title>
-        <b-list-group flush>
-          <b-list-group-item
-            href="https://sass-lang.com/"
-            v-b-tooltip.hover.right
-            title="A mature, stable, and powerful professional grade CSS extension language"
-          >
-            Sass
-          </b-list-group-item>
-          <b-list-group-item
-            href="https://bootstrap-vue.js.org/"
-            v-b-tooltip.hover.right
-            title="One of the most comprehensive implementations of the Bootstrap 4.3 component and grid system for Vue.js"
-          >
-            Bootstrap Vue
-          </b-list-group-item>
-          <b-list-group-item
-            href="https://gridsome.org/plugins/@gridsome/plugin-critical"
-            v-b-tooltip.hover.right
-            title="Extracts & inlines critical-path (above-the-fold) CSS"
-          >
-            Critical path CSS extraction
-          </b-list-group-item>
-        </b-list-group>
-      </b-card>
-    </b-card-group>
+      <b-card-group deck>
+        <b-card img-src="../../uploads/adjustments.svg" img-alt="Data" img-left>
+          <b-card-title>Data</b-card-title>
+          <b-list-group flush>
+            <b-list-group-item
+              href="https://www.netlify.com/docs/form-handling/"
+              v-b-tooltip.hover.right
+              title="Form submission handling provided by Netlify"
+            >
+              Netlify Forms
+            </b-list-group-item>
+            <b-list-group-item
+              href="https://analytics.google.com/analytics/web/"
+              v-b-tooltip.hover.right
+              title="A web analytics service offered by Google that tracks and reports website traffic"
+            >
+              Google Analytics
+            </b-list-group-item>
+            <b-list-group-item
+              href="https://gridsome.org/plugins/@gridsome/plugin-sitemap"
+              v-b-tooltip.hover.right
+              title="Automatic sitemap generation for Gridsome sites"
+            >
+              Sitemap generator
+            </b-list-group-item>
+          </b-list-group>
+        </b-card>
+      </b-card-group>
+    </section>
 
-    <b-card-group deck>
-      <b-card img-src="../../uploads/adjustments.svg" img-alt="Data" img-left>
-        <b-card-title>Data</b-card-title>
-        <b-list-group flush>
-          <b-list-group-item
-            href="https://www.netlify.com/docs/form-handling/"
-            v-b-tooltip.hover.right
-            title="Form submission handling provided by Netlify"
-          >
-            Netlify Forms
-          </b-list-group-item>
-          <b-list-group-item
-            href="https://analytics.google.com/analytics/web/"
-            v-b-tooltip.hover.right
-            title="A web analytics service offered by Google that tracks and reports website traffic"
-          >
-            Google Analytics
-          </b-list-group-item>
-          <b-list-group-item
-            href="https://gridsome.org/plugins/@gridsome/plugin-sitemap"
-            v-b-tooltip.hover.right
-            title="Automatic sitemap generation for Gridsome sites"
-          >
-            Sitemap generator
-          </b-list-group-item>
-        </b-list-group>
-      </b-card>
-    </b-card-group>
+    <SVGPolygon :height="svgHeight" :triangles="12" />
   </Layout>
 </template>
 
 <script>
+import SVGPolygon from '../components/SVGPolygon.vue';
+
 export default {
+  components: {
+    SVGPolygon,
+  },
+  data: () => {
+    return {
+      svgHeight: 0,
+    };
+  },
+  mounted() {
+    this.$nextTick(() => {
+      const element = document.getElementById('about');
+      const rect = element.getBoundingClientRect();
+      this.svgHeight = rect.y + rect.height + window.scrollY;
+    });
+  },
   metaInfo: {
     title: 'Gridsome starter bootstrap',
   },
@@ -200,13 +221,18 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+#about {
+  margin-bottom: 8rem;
+  padding-bottom: 4rem;
+}
+
 .card {
   box-shadow: 2px 3px 4px #888;
   transition: all 0.2s ease-in-out;
 
   &:hover {
     box-shadow: 4px 6px 8px #aaa;
-    transform: scale(1.025);
+    transform: scale(1.02);
   }
 }
 
