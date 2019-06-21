865b93a32375d60f21e1b35440cc00bf4d86dc67
Russell Chapman <chpmnrssll@gmail.com>
2019-06-20 22:14:41 -0600

    Post contact form to Netlify with AJAX

--- src/components/Agency/ContactForm.vue
+++ src/components/Agency/ContactForm.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="d-flex flex-column justify-content-around">
-    <div class="container text-center">
+    <div class="container my-5 py-5">
       <SequentialEntrance animation="entranceFromTop" class-name="row py-4">
         <div class="col-sm text-center">
           <g-image class="pb-4" src="~/assets/images/address-icon.png" alt="Address" />
@@ -18,7 +18,84 @@
           <p>123 455 755</p>
         </div>
       </SequentialEntrance>
-      <hr />
+
+      <hr class="divider" />
+
+      <div class="container d-flex flex-column mt-5 pt-5 text-center">
+        <h2 class="font-weight-bold mb-4">GRAVIDA NIBH VEL 5</h2>
+        <h1 class="font-weight-bold mb-4">Send Us a Message</h1>
+        <hr class="my-4" />
+      </div>
+
+      <b-form
+        @submit.prevent="onSubmit"
+        method="post"
+        name="contact"
+        action="/agency/thanks/"
+        data-netlify="true"
+        data-netlify-recaptcha="true"
+        data-netlify-honeypot="bot-field"
+      >
+        <input type="hidden" name="form-name" value="contact" />
+        <input type="hidden" name="bot-field" />
+
+        <b-form-group :state="validateName">
+          <label for="contact-name">Name</label>
+          <b-input
+            @blur="enteredField.name = true"
+            :state="validateName"
+            v-model="formData.name"
+            id="contact-name"
+          />
+          <b-form-invalid-feedback :state="validateName">
+            This field is required.
+          </b-form-invalid-feedback>
+        </b-form-group>
+
+        <b-form-group :state="validateEmail">
+          <label for="contact-email">Email</label>
+          <b-input
+            @blur="enteredField.email = true"
+            :state="validateEmail"
+            v-model="formData.email"
+            id="contact-email"
+          />
+          <b-form-invalid-feedback :state="validateEmail">
+            This field is required.
+          </b-form-invalid-feedback>
+        </b-form-group>
+
+        <b-form-group :state="validateSubject">
+          <label for="contact-subject">Subject</label>
+          <b-input
+            @blur="enteredField.subject = true"
+            :state="validateSubject"
+            v-model="formData.subject"
+            id="contact-subject"
+          />
+          <b-form-invalid-feedback :state="validateSubject">
+            This field is required.
+          </b-form-invalid-feedback>
+        </b-form-group>
+
+        <b-form-group :state="validateMessage">
+          <label for="contact-message">Message</label>
+          <b-form-textarea
+            id="contact-message"
+            @blur="enteredField.message = true"
+            :state="validateMessage"
+            v-model="formData.message"
+            rows="3"
+            max-rows="6"
+          />
+          <b-form-invalid-feedback :state="validateMessage">
+            This field is required.
+          </b-form-invalid-feedback>
+        </b-form-group>
+
+        <div data-netlify-recaptcha="true" />
+        <b-button type="submit" variant="primary">Submit</b-button>
+      </b-form>
     </div>
   </section>
 </template>
@@ -29,6 +106,60 @@ import SequentialEntrance from './SequentialEntrance.vue';
 export default {
   components: {
     SequentialEntrance,
+  },
+  data() {
+    return {
+      formData: {
+        name: '',
+        email: '',
+        subject: '',
+        message: '',
+      },
+      enteredField: {
+        name: false,
+        email: false,
+        subject: false,
+        message: false,
+      },
+    };
+  },
+  computed: {
+    validateName() {
+      return this.enteredField.name ? this.formData.name.length > 0 : null;
+    },
+    validateEmail() {
+      return this.enteredField.email ? this.formData.email.length > 0 : null;
+    },
+    validateSubject() {
+      return this.enteredField.subject ? this.formData.subject.length > 0 : null;
+    },
+    validateMessage() {
+      return this.enteredField.message ? this.formData.message.length > 0 : null;
+    },
+  },
+  methods: {
+    // onSubmit(event) {
+    //   event.preventDefault();
+    //   alert(JSON.stringify(this.formData));
+    // },
+    encode(data) {
+      return Object.keys(data)
+        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+        .join('&');
+    },
+    onSubmit(event) {
+      const action = event.target.getAttribute('action');
+      fetch(action, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: this.encode({
+          'form-name': event.target.getAttribute('name'),
+          ...this.formData,
+        }),
+      })
+        .then(() => this.$router.push(action))
+        .catch(error => alert(error));
+    },
   },
 };
 </script>
@@ -65,5 +196,16 @@ p {
   color: #8192aa;
   font-family: 'Nunito', sans-serif;
   font-size: 100%;
+}
+label {
+  color: #8192aa;
+  font-size: 100%;
+  font-weight: 700;
+}
+.divider {
+  border-top-width: 1px;
+  border-top-color: #8192aa;
+  opacity: 0.5;
+  width: 100%;
 }
 </style>
---
+++ src/pages/Agency/thanks.vue
@@ -0,0 +1,50 @@
+<template>
+  <AgencyLayout>
+    <FullScreenHero>
+      <template v-slot:background>
+        <g-image src="~/assets/images/hero-bg.png" alt="" immediate />
+      </template>
+      <template v-slot:body>
+        <h1>Thank You</h1>
+        <p>
+          Thanks for contacting us! We will be in touch with you shortly.
+        </p>
+        <b-button @click="openCalendlyWidget" pill variant="primary">Schedule Meeting</b-button>
+      </template>
+    </FullScreenHero>
+
+    <JoinTheClub />
+  </AgencyLayout>
+</template>
+
+<script>
+import FullScreenHero from '../../components/Agency/FullScreenHero.vue';
+import JoinTheClub from '../../components/Agency/JoinTheClub.vue';
+
+export default {
+  components: {
+    FullScreenHero,
+    JoinTheClub,
+  },
+  metaInfo: {
+    title: 'About',
+  },
+  methods: {
+    openCalendlyWidget() {
+      window.Calendly.initPopupWidget({ url: 'https://calendly.com/chpmnrssll' });
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+:root {
+  --primaryBlue: #0f4ea5;
+  --primaryBlueA: #0f4ea588;
+  --primaryGreen: #0fa597;
+  --primaryGreenA: #0fa59788;
+}
+section {
+  transition: 0.25s filter ease-in-out;
+}
+</style>
